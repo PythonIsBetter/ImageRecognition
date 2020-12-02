020f260e0742a13e2e5b5f92fee1ecdda88d16b5
YangBiao <biaoy@outlook.jp>
2020-12-02 15:05:25 +0900

    add Japanese comment

--- main.py
+++ main.py
@@ -4,17 +4,20 @@ import pytesseract
 import cv2 as cv
 
 
+# 最も基礎的な方法
 def way_1(path, lan):
-    text = pytesseract.image_to_string(Image.open(path), lang=lan)
+    text = pytesseract.image_to_string(Image.open(path), lang=lan)  # 画像を読込
+
     print('-----------------------WAY 1---------------------------\n')
     print(text)
     print('--------------------------------------------------\n\n\n')
 
 
 # 灰度处理
+# グレースケール化
 def way_2(path, lan):
     img = Image.open(path)
-    img = img.convert('RGB')  # 这里也可以尝试使用L
+    img = img.convert('RGB')
     enhancer = ImageEnhance.Color(img)
     enhancer = enhancer.enhance(0)
     enhancer = ImageEnhance.Brightness(enhancer)
@@ -23,20 +26,25 @@ def way_2(path, lan):
     enhancer = enhancer.enhance(8)
     enhancer = ImageEnhance.Sharpness(enhancer)
     img = enhancer.enhance(20)
+
     text = pytesseract.image_to_string(img, lang=lan)
+
     print('-----------------------WAY 2---------------------------\n')
     print(text)
     print('--------------------------------------------------\n\n\n')
 
 
-# 识别前处理
 # 图片二值化
+# 二値化法
 def way_3(path, lan):
     img = Image.open(path)
-    # 模式L”为灰色图像，它的每个像素用8个bit表示，0表示黑，255表示白，其他数字表示不同的灰度。
+
+    # L为灰色图像，0表示黑，255表示白，其他数字表示不同的灰度
+    # Lはグレー色の画像
     Img = img.convert('L')
 
     # 自定义灰度界限，大于这个值为黑色，小于这个值为白色
+    # しきい値を指定。大きい場合は黒、小さい場合は白
     threshold = 200
 
     table = []
@@ -48,24 +56,25 @@ def way_3(path, lan):
 
     # 图片二值化
     photo = Img.point(table, '1')
-    # photo.save("test2.png")
-    # # 识别图片内容
-    # img_path = 'test2.png'
+
     text = pytesseract.image_to_string(photo, lang=lan)
+
     print('-----------------------WAY 3---------------------------\n')
     print(text)
     print('--------------------------------------------------\n\n\n')
 
 
-# 用了opencv
+# opencv的大津二值化法
+# opencvでの大津の二値化法
 def way_4(path, lan):
     img = cv.imread(path)
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
     ret, binary = cv.threshold(gray, 0, 255, cv.THRESH_BINARY_INV | cv.THRESH_OTSU)
-    # 此处可根据图像效果进行相应的形态学处理，再进行识别，提升准确率
     cv.bitwise_not(binary, binary)
 
     textImg = Image.fromarray(binary)
+
     text = pytesseract.image_to_string(textImg, lang=lan)
     print('-----------------------WAY 4---------------------------\n')
     print(text)
